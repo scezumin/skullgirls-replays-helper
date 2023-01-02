5c058c737f61fdbbcac4dfec049d7c08f62b544d
scezumin <scezumin@gmail.com>
2023-01-01 23:01:41 -0800

    pretty prefs

--- preferences_TEMPLATE.py
+++ preferences_TEMPLATE.py
@@ -1,11 +1,21 @@
+######################
+# SYSTEM-LEVEL PREFS #
+######################
 REPLAYS_DIRECTORY = r"C:\Users\Scezumin\Documents\Skullgirls\Replays_SG2EPlus\76561197993372680"
 OWNER_NAME = "Scezumin"
-NAME_LENGTH_LIMIT = 13
 INPUT_FILENAME = r"C:\Users\Scezumin\PycharmProjects\skullgirls-replays-helper\input.txt"
 OUTPUT_FILENAME = r"C:\Users\Scezumin\PycharmProjects\skullgirls-replays-helper\output.txt"
+
+####################
+# INPUT FILE PREFS #
+####################
 COMMENT_PREFIX = "#"
 
-# Note: it is recommended that all of the below values be the same length.
+#####################
+# OUTPUT FILE PREFS #
+#####################
+NAME_LENGTH_LIMIT = 13
+# Note: it is recommended that all the below values be the same length.
 EMPTY_FIGHTER = "..."
 FIGHTER_ABBREVIATIONS = {
     "Annie": "Ann",
